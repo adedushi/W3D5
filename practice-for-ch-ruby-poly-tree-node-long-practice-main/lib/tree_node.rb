9eefa685126c9ea5d894b27065981b96b04ca39e
Albert Dedushi <adedushi@hotmail.com>
2023-11-03 15:03:45 -0400

    Complete #parent=

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,18 +1,25 @@
 class PolyTreeNode
     attr_reader :value, :parent, :children
+    attr_writer :children
 
     def initialize(value)
         @value = value
         @parent = nil
         @children = []
     end
-    def parent=(new_parent)
-        @parent = new_parent
-       if !new_parent.children.include?(self) && self.parent.children != nil 
-       new_parent.children<<self     
-       end
-    end
-end
 
-a = PolyTreeNode.new("a")
-puts a
+    def parent=(new_parent)
+        old_parent = @parent
+        @parent = new_parent
+
+        if !old_parent.nil?
+            old_parent.children.delete(self)
+        end
+
+        if new_parent != nil && !new_parent.children.include?(self)
+            new_parent.children << self  
+        end
+        
+    end
+
+end
