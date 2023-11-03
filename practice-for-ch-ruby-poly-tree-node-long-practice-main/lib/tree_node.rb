7adab867c9cac18717c30eda5d493c07608ab066
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-03 14:23:11 -0400

    half of parent=

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -6,8 +6,12 @@ class PolyTreeNode
         @parent = nil
         @children = []
     end
-
-
+    def parent=(new_parent)
+        @parent = new_parent
+       if !new_parent.children.include?(self) && self.parent.children != nil 
+       new_parent.children<<self     
+       end
+    end
 end
 
 a = PolyTreeNode.new("a")
