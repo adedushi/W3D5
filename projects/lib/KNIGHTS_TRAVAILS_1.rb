c78e9259ba8af1a45b16b6b274688d6504c00e46
Albert Dedushi <adedushi@hotmail.com>
2023-11-06 14:50:15 -0500

    Add #build_move_tree

--- projects/lib/KNIGHTS_TRAVAILS_1.rb
+++ projects/lib/KNIGHTS_TRAVAILS_1.rb
@@ -1,3 +1,4 @@
+require "byebug"
 require_relative 'tree_node'
 class KnightPathFinder
     attr_accessor :pos, :board, :considered_positions
@@ -14,8 +15,8 @@ class KnightPathFinder
         [x - 1, y - 2], 
         [x - 2, y - 1])
 
+
         array.select!{|subs| subs.all?{|el| el < 8 && el >= 0}}
-      
 
     end
 
@@ -25,21 +26,29 @@ class KnightPathFinder
         @board = Array.new(8) {Array.new(8,"")}
         @considered_positions = [@pos]
     end
+
     def new_move_positions(pos)
-        considered_positions << pos if !considered_positions.include?(pos) 
-        return valid_moves(pos)
+        
+        valid_moves = KnightPathFinder.valid_moves(pos)
+        valid_moves.select!{ |move| !considered_positions.include?(move)}
+        considered_positions += valid_moves
+        valid_moves
+
     end
+
     def build_move_tree
         queue=[@root_node]
         while !queue.empty?
-            el=queue.shift
-            if el.pos == [7,7]
-                return true 
-            else
-                queue << valid_moves(el)
+            el = queue.shift
+            possible_moves = new_move_positions(el.value)
+            possible_moves.each do |move|
+                node = PolyTreeNode.new(move)
+                el.add_child(node)
+                queue << node
             end
         end
-        return nil 
     end
+
+
 end
 
