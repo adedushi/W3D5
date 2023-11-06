7a8baf0d5fe4bda988d1d6a635265b9d6a41d6b9
Albert Dedushi <adedushi@hotmail.com>
2023-11-06 13:34:51 -0500

    Fix ::valid_moves

--- projects/lib/KNIGHTS_TRAVAILS_1.rb
+++ projects/lib/KNIGHTS_TRAVAILS_1.rb
@@ -1,21 +1,30 @@
 require_relative 'tree_node'
 class KnightPathFinder
-    def self.valid_moves(pos)
-        array=[]
+    attr_accessor :pos, :board, :considered_positions
+
+    def valid_moves(pos)
+        array = []
         x, y = pos 
-        array.push([x + 1, y + 2], [x + 2, y + 1],[x + 2, y - 1],[x + 1, y - 2],[x - 1, y + 2],[x - 2, y + 1],[x - 1, y - 2], [x - 2, y - 1])
-        array.select!{|subs| subs.any?{|el| el < 8 && el >= 0}}
-        if array.empty?
-         return false 
-        else
-            return true
-        end
+        array.push([x + 1, y + 2], 
+        [x + 2, y + 1],
+        [x + 2, y - 1],
+        [x + 1, y - 2],
+        [x - 1, y + 2],
+        [x - 2, y + 1],
+        [x - 1, y - 2], 
+        [x - 2, y - 1])
+
+        array.select!{|subs| subs.all?{|el| el < 8 && el >= 0}}
+        print array
+
     end
 
     def initialize(pos)
-        @pos =pos 
-        @board = Array.new(8) {Array.new(8) {[]}}
-        @considered_positions=[@pos]
+        @pos = pos
+        @root_node = PolyTreeNode.new(@pos) 
+        @board = Array.new(8) {Array.new(8,"")}
+        @considered_positions = [@pos]
     end
         
 end
+
